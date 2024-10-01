ca1c15e088287a7b629b02bbb3de24ad2115dcee
Harry2904 <123268915+Harry101969@users.noreply.github.com>
2024-10-01 20:32:43 +0530

    Delete turboapp directory

--- turboapp/packages/common/src/index.ts
+++
@@ -1,4 +0,0 @@
-export const VALUE = "Harsh Agarwal";
-console.log(VALUE);
-export const BACKEND_URL = "https://google.com";
-console.log(BACKEND_URL)
--- turboapp/packages/ui/src/admin.tsx
+++
@@ -1,3 +0,0 @@
-export function Admin() {
-  return <div>Admin Page</div>;
-}
