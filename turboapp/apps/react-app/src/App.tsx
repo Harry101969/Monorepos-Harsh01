ddf7ccfb8c7da86652223c0f76c24837df597c8c
Harry101969 <harshagarwal19696@gmail.com>
2024-10-01 15:54:53 +0530

    Updated

---
+++ turboapp/apps/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@repo/ui/button";
+function App() {
+  return (
+    <>
+      <div>
+        <Button appName="web" children="Hello Harsh" />
+      </div>
+    </>
+  );
+}
+
+export default App;
